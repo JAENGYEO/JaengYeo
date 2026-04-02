8168b87ae708fbbf1ace99f03b9c6fa02e6118a4
Bin <ybin4548@gmail.com>
2026-04-02 17:02:28 +0900

    📝docs: ProductEntity 추가 ( 아이템 )
    - id(UUID) : 고유 식별자
    - userId(String) :  소유자 user_id
    - quantity(Integer32) : 현재 수량
    - quantityUnit(String) : 단위
    - mainCategory(String) : 대분류
    - midCategory(UUID) : 중분류 ID
    - subCategory(UUID) : 소분류 ID
    - purchaseDate(Date) : 구매일자
    - expiryDate(Date) : 유통기한
    - price(Integer32) : 가격
    - locationMemo(String) : 위치 메모
    - memo(String) : 일반 메모
    - imageUrl(String) : Supabase Storage URL
    - isClassified(Boolean) : 분류/ 미분류 상태
    - isLowStockNotificationEnabled(Boolean) : 재고 임박 알림 활성화 여부
    - lowStockThreshold(Integer32) : 재고 임박 기준 수량 (isLowStockNotificationEnabled : false일 경우 무시)
    - isFavorite(Boolean) : 즐겨찾기 (v1.1 위젯용)
    - createdAt(Date) : 생성일
    - updatedAt(Date) : 수정일
    - syncStatus(String) : 동기화 상태

---
+++ JaengYeo/CoreData/ProductEntity+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  ProductEntity+CoreDataClass.swift
+//  JaengYeo
+//
+//  Created by 손영빈 on 4/2/26.
+//
+//
+
+public import Foundation
+public import CoreData
+
+public typealias ProductEntityCoreDataClassSet = NSSet
+
+@objc(ProductEntity)
+public class ProductEntity: NSManagedObject {
+
+}
---
+++ JaengYeo/CoreData/ProductEntity+CoreDataProperties.swift
@@ -0,0 +1,47 @@
+//
+//  ProductEntity+CoreDataProperties.swift
+//  JaengYeo
+//
+//  Created by 손영빈 on 4/2/26.
+//
+//
+
+public import Foundation
+public import CoreData
+
+
+public typealias ProductEntityCoreDataPropertiesSet = NSSet
+
+extension ProductEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ProductEntity> {
+        return NSFetchRequest<ProductEntity>(entityName: "ProductEntity")
+    }
+
+    @NSManaged public var id: UUID
+    @NSManaged public var userId: String
+    @NSManaged public var name: String
+    @NSManaged public var quantity: Int32
+    @NSManaged public var quantityUnit: String?
+    @NSManaged public var mainCategory: String
+    @NSManaged public var midCategoryId: UUID?
+    @NSManaged public var subCategoryId: UUID?
+    @NSManaged public var purchaseDate: Date?
+    @NSManaged public var expiryDate: Date?
+    @NSManaged public var price: Int32
+    @NSManaged public var locationMemo: String?
+    @NSManaged public var memo: String?
+    @NSManaged public var imageUrl: String?
+    @NSManaged public var isClassified: Bool
+    @NSManaged public var lowStockThreshold: Int32
+    @NSManaged public var isFavorite: Bool
+    @NSManaged public var createdAt: Date
+    @NSManaged public var updatedAt: Date
+    @NSManaged public var syncStatus: String
+    @NSManaged public var isLowStockNotificationEnabled: Bool
+
+}
+
+extension ProductEntity : Identifiable {
+
+}
