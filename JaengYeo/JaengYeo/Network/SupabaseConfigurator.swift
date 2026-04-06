58183d94169fe4975f7149f23c555e912a79054e
Bin <ybin4548@gmail.com>
2026-04-06 19:53:51 +0900

    ✨feat: SupabaseConfigurator 구현

---
+++ JaengYeo/JaengYeo/Network/SupabaseConfigurator.swift
@@ -0,0 +1,22 @@
+//
+//  SupabaseConfigurator.swift
+//  JaengYeo
+//
+//  Created by 손영빈 on 4/6/26.
+//
+
+import Foundation
+import Supabase
+
+func makeSupabaseClient() -> SupabaseClient {
+    guard let urlString = Bundle.main.infoDictionary?["SUPABASE_URL"] as? String,
+          let url = URL(string: urlString),
+          let key = Bundle.main.infoDictionary?["SUPABASE_ANON_KEY"] as? String else {
+        fatalError("Supabase URL 또는 Key가 Info.plist에 설정되지 않았습니다.")
+    }
+    
+    return SupabaseClient(
+        supabaseURL: url,
+        supabaseKey: key
+    )
+}
