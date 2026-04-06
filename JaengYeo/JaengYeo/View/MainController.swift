8db4a6bbbaa7669df450cf3203ee2437db529dc4
Bin <ybin4548@gmail.com>
2026-04-06 10:07:12 +0900

    ✨feat: MainController 추가 및 연결
    - MVP 단계에서 TabBar는 3개로 구성(홈, 등록, 재고)
    - 연결 VC는 수정 필요
    - tabBar.tintColor, unselectedItemTintColor 수정 필요

---
+++ JaengYeo/JaengYeo/View/MainController.swift
@@ -0,0 +1,35 @@
+//
+//  MainController.swift
+//  JaengYeo
+//
+//  Created by 손영빈 on 4/6/26.
+//
+
+import UIKit
+
+class MainController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupTabBar()
+    }
+}
+
+extension MainController {
+    private func setupTabBar() {
+        let homeVC = createNC(rootVC: ViewController(), title: "홈", image: "house")
+        let registerVC = createNC(rootVC: ViewController(), title: "등록", image: "camera")
+        let stockVC = createNC(rootVC: ViewController(), title: "재고", image: "bag")
+//        tabBar.tintColor =
+//        tabBar.unselectedItemTintColor =
+        viewControllers = [homeVC, registerVC, stockVC]
+    }
+    
+    private func createNC(rootVC: UIViewController, title: String, image: String) -> UINavigationController {
+        let nc = UINavigationController(rootViewController: rootVC)
+        nc.tabBarItem.title = title
+        nc.tabBarItem.image = UIImage(systemName: image)
+        return nc
+    }
+}
