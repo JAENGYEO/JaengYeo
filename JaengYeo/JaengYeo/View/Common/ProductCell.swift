18beae836736644e6feeba408b406e7abc545f4a
hanjuheon <godzx3@gmail.com>
2026-04-08 20:24:14 +0900

    🌟feat: ProductCell 구현
    - UICollectionViewListCell 기반 공용 상품 셀 구현
    - 셀 타입별 UI 분기 처리
    - 상품 정보 업데이트 로직 추가
    - 설명 스택 동적 구성 로직 반영
    
    ref #32

--- JaengYeo/JaengYeo/View/Common/ProductCell.swift
+++ JaengYeo/JaengYeo/View/Common/ProductCell.swift
@@ -8,89 +8,250 @@
 import UIKit
 import SnapKit
 import Then
-import RxCocoa
 
-final class ProductCell: UICollectionViewCell {
+/// 공용 상품 셀
+final class ProductCell: UICollectionViewListCell{
     
-    //MARK: - Componenets
+    //MARK: - Enum
+    /// 상품 셀 디자인 타입
+    enum ProductCellType {
+        /// 기본 디자인
+        case defaultType
+        /// 상품 추가 정보 표기 디자인
+        case detailType
+        /// 등록 디자인
+        case registType
+    }
+    
+    // MARK: - Properties
+    private var cellType: ProductCellType
+    
+    // MARK: - Components
     /// 아이템 이미지 뷰
-    let productImageView = UIImageView().then {
+    private let productImageView = UIImageView().then {
         $0.contentMode = .scaleAspectFill
         $0.clipsToBounds = true
         $0.layer.cornerRadius = 8
+        $0.backgroundColor = .accent
     }
     
     /// 아이템 타이틀
-    let productTitleLabel = StyledLabel(config: .bodyMedium14)
+    private let productTitleLabel = StyledLabel(config: .bodyMedium14).then {
+        $0.text = "아이템 타이틀"
+    }
     
     /// 아이템 카운트
-    let productCountLabel = StyledLabel(config: .titleSemi18)
+    private let productCountLabel = StyledLabel(config: .titleSemi18).then {
+        $0.text = "0"
+    }
     
-    /// 아이템 정보 스텍
-    let productDesciptStack = UIStackView().then {
+    /// 카운트 단위
+    private let countUnitLabel = StyledLabel(config: .body12.updatingColor(color: .gray300)).then {
+        $0.text = "개"
+    }
+    
+    /// 메인 스택
+    private let productMainStack = UIStackView().then {
         $0.axis = .horizontal
+        $0.spacing = 16
+        $0.alignment = .center
+        $0.distribution = .fill
+    }
+    
+    /// 정보 스택
+    private let productInfoStack = UIStackView().then {
+        $0.axis = .vertical
         $0.spacing = 4
         $0.alignment = .leading
-        $0.distribution = .equalSpacing
+        $0.distribution = .fill
     }
     
-    //MARK: - Init
-    override init(frame: CGRect) {
-        super.init(frame: .zero)
+    /// 메인 설명 스택
+    private let productDescriptionStack = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 4
+        $0.alignment = .center
+        $0.distribution = .fill
     }
     
+    /// 서브 설명 스택
+    private let productSubDescriptionStack = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 4
+        $0.alignment = .center
+        $0.distribution = .fill
+    }
+    
+    /// 수량 뷰
+    private let productCountView = UIView()
+    
+    // MARK: - Init
+    init(frame: CGRect = .zero, type: ProductCellType) {
+        self.cellType = type
+        super.init(frame: frame)
+        configureUI()
+    }
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    convenience init(type: ProductCellType) {
+        self.init(frame: .zero, type: type)
+    }
 }
 
+// MARK: - Public
 extension ProductCell {
-    
-    func configureUI() {
-     
-        let productMainStack = UIStackView().then {
-            $0.axis = .horizontal
-            $0.spacing = 16
-            $0.alignment = .leading
-            $0.distribution = .fillProportionally
+    /// Update UI 메소드
+    func updateUI(
+        title: String,
+        freshness: Int?,
+        descriptions: [String]?,
+        subdescriptions: [String]?,
+        count: Int?
+    ) {
+        productTitleLabel.text = title
+        
+        updateDescriptionStack(
+            stackView: productDescriptionStack,
+            freshness: freshness,
+            texts: descriptions,
+        )
+        
+        if cellType != .registType, let count = count {
+            productCountLabel.text = "\(count)"
         }
         
-        let productInfoStack = UIStackView().then {
-            $0.axis = .vertical
-            $0.spacing = 4
-            $0.alignment = .leading
-            $0.distribution = .fillProportionally
-        }
-    
-        let productCountStack = UIStackView().then {
-            $0.axis = .horizontal
-            $0.spacing = 0
-            $0.alignment = .center
-            $0.distribution = .equalSpacing
+        if cellType == .detailType {
+            updateDescriptionStack(
+                stackView: productSubDescriptionStack,
+                freshness: nil,
+                texts: subdescriptions,
+            )
         }
         
-        let countLabel = StyledLabel(config: .body12.updatingColor(color: .gray300))
         
-        productCountStack.addArrangedSubview(productCountLabel)
-        productCountStack.addArrangedSubview(countLabel)
+    }
+}
+
+// MARK: - Update
+private extension ProductCell {
+    /// 설명 스택에 삽입될 텍스트 라벨 제작 메소드
+    private func updateDescriptionStack(
+        stackView: UIStackView,
+        freshness: Int?,
+        texts: [String]?,
+    ) {
+        var labels: [UILabel] = []
         
-        productInfoStack.addArrangedSubview(productTitleLabel)
-        productInfoStack.addArrangedSubview(productDesciptStack)
-        productMainStack.addArrangedSubview(productImageView)
-        productMainStack.addArrangedSubview(productInfoStack)
+        /// 유통기한 텍스트 생성
+        if let freshness {
+            let freshnessLabel = StyledLabel(
+                config: .body12.updatingColor(color: .primaryRed)
+            ).then {
+                $0.text = freshness > 0 ? "\(freshness)일 남음" : "소비기한 만료"
+                $0.numberOfLines = 1
+            }
+            labels.append(freshnessLabel)
+        }
         
-        contentView.addSubview(productInfoStack)
-        contentView.addSubview(productCountStack)
+        /// 설명 라벨 생성
+        texts?.forEach { text in
+            let label = StyledLabel(config: .body12.updatingColor(color: .gray300)).then {
+                $0.text = text
+                $0.numberOfLines = 1
+            }
+            labels.append(label)
+        }
         
-        
-        productMainStack.snp.makeConstraints{
-            $0.top.equalToSuperview().offset(12)
-            $0.bottom.equalToSuperview().inset(12)
-            $0.leading.equalToSuperview().offset(16)
+        /// 스택뷰에 삽입
+        labels.enumerated().forEach { index, label in
+            stackView.addArrangedSubview(label)
+            
+            // dot 라벨 추가
+            if index < labels.count - 1 {
+                let dotLabel = StyledLabel(config: .body12.updatingColor(color: .gray300)).then {
+                    $0.text = "·"
+                    $0.numberOfLines = 1
+                }
+                stackView.addArrangedSubview(dotLabel)
+            }
         }
     }
 }
 
-#Preview{
-    ProductCell()
+
+// MARK: - Layout
+private extension ProductCell {
+    
+    func configureUI() {
+        contentView.addSubview(productMainStack)
+        contentView.addSubview(productCountView)
+        
+        productMainStack.addArrangedSubview(productImageView)
+        productMainStack.addArrangedSubview(productInfoStack)
+        
+        productInfoStack.addArrangedSubview(productTitleLabel)
+        productInfoStack.addArrangedSubview(productDescriptionStack)
+        productInfoStack.addArrangedSubview(productSubDescriptionStack)
+        
+        productCountView.addSubview(productCountLabel)
+        productCountView.addSubview(countUnitLabel)
+        
+        productMainStack.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(12)
+            $0.bottom.equalToSuperview().inset(12)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.lessThanOrEqualTo(productCountView.snp.leading).offset(-12)
+        }
+        
+        productCountView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(16)
+        }
+        
+        productImageView.snp.makeConstraints {
+            $0.size.equalTo(64)
+        }
+        
+        productCountLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview()
+            $0.trailing.equalTo(countUnitLabel.snp.leading).offset(-2)
+            $0.lastBaseline.equalTo(countUnitLabel.snp.lastBaseline)
+        }
+        
+        countUnitLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview()
+            $0.centerY.equalToSuperview()
+        }
+        
+        switch cellType {
+        case .defaultType:
+            productSubDescriptionStack.isHidden = true
+            productCountView.isHidden = false
+            
+        case .detailType:
+            productSubDescriptionStack.isHidden = false
+            productCountView.isHidden = false
+            
+        case .registType:
+            productSubDescriptionStack.isHidden = true
+            productCountView.isHidden = true
+        }
+    }
+}
+
+
+
+#Preview {
+    let cell = ProductCell(type: .detailType)
+    cell.updateUI(
+        title: "바나나",
+        freshness: -1,
+        descriptions: ["실온", "과일"],
+        subdescriptions: ["추가 정보", "라벨4"],
+        count: 6
+    )
+    return cell
 }
