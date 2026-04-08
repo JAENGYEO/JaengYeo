8146d40c72e6ff5a2540bf62117c783f1851aea9
hanjuheon <godzx3@gmail.com>
2026-04-08 10:24:32 +0900

    🌟 feat: StyledButton 제작
    - ConfigurableButton 기반 StyledButton 구현
    - 버튼 타이틀 스타일 및 외형 스타일 적용
    - normal, highlighted, disabled 상태별 타이틀 스타일 반영
    - 버튼 상태에 따른 배경색 변경 로직 추가
    
    ref #32

--- JaengYeo/JaengYeo/Utils/Base/StyledLabel.swift
+++ JaengYeo/JaengYeo/Utils/Base/StyledLabel.swift
@@ -10,6 +10,7 @@ import UIKit
 /// 스타일 라벨 컴포넌트
 final class StyledLabel: UILabel, ConfigurableLabel {
     
+    // MARK: - Properites
     /// Label Configuration
     var labelConfiguration: LabelConfiguration? {
         didSet {
@@ -24,13 +25,15 @@ final class StyledLabel: UILabel, ConfigurableLabel {
         }
     }
     
-    
+    // MARK: - Init
     convenience init(config: LabelConfiguration) {
         self.init()
         self.labelConfiguration = config
     }
-    
-    
+}
+
+// MARK: - ConfigurableLabel setting
+extension StyledLabel{
     /// Text Configuration 설정 메소드
     func reapplyConfiguration() {
         guard let config = labelConfiguration else { return }
--- JaengYeo/JaengYeo/Utils/Configuration/ButtonConfiguration.swift
+++ JaengYeo/JaengYeo/Utils/Configuration/ButtonConfiguration.swift
@@ -6,6 +6,7 @@
 //
 import UIKit
 
+/// Button Ttitle Configuration 구조체
 struct ButtonTitleConfiguration {
     let font: UIFont
     let normalColor: UIColor
@@ -13,14 +14,15 @@ struct ButtonTitleConfiguration {
     let disabledColor: UIColor
     let kern: CGFloat
     
+    /// 색상 변환 메소드
     func updatingColor(
-           color: UIColor? = nil,
+           normalColor: UIColor? = nil,
            highlightedColor: UIColor? = nil,
            disabledColor: UIColor? = nil
        ) -> ButtonTitleConfiguration {
            .init(
                font: font,
-               normalColor: color ?? self.normalColor,
+               normalColor: normalColor ?? self.normalColor,
                highlightedColor: highlightedColor ?? self.highlightedColor,
                disabledColor: disabledColor ?? self.disabledColor,
                kern: kern
@@ -28,26 +30,41 @@ struct ButtonTitleConfiguration {
        }
 }
 
+/// Button Appearance Configuration 구조체
 struct ButtonAppearanceConfiguration {
     let backgroundColor: UIColor
-    let highlightBackgroundColor: UIColor
+    let highlightedBackgroundColor: UIColor
     let disabledBackgroundColor: UIColor
     let cornerRadius: CGFloat
     let borderWidth: CGFloat
     let borderColor: UIColor
+    
+    /// 색상 변환 메소드
+    func updatingColor(
+        backgroundColor: UIColor? = nil,
+        highlightedBackgroundColor: UIColor? = nil,
+        disabledBackgroundColor: UIColor? = nil
+    ) -> ButtonAppearanceConfiguration {
+        .init(backgroundColor: backgroundColor ?? self.backgroundColor,
+              highlightedBackgroundColor: highlightedBackgroundColor ?? self.highlightedBackgroundColor,
+              disabledBackgroundColor: disabledBackgroundColor ?? self.disabledBackgroundColor,
+              cornerRadius: cornerRadius,
+              borderWidth: borderWidth,
+              borderColor: borderColor
+        )
+    }
 }
 
-struct ButtonConfiguration {
-    let titleNormal: ButtonTitleConfiguration
-    let appearance: ButtonAppearanceConfiguration
+protocol ConfigurableButton {
+    var titleConfiguration: ButtonTitleConfiguration? { get set }
+    var appearanceConfiguration: ButtonAppearanceConfiguration? { get set }
 }
-
 
 // MARK: - Setting ButtonTitleConfiguration
-// MARK: - Title
 extension ButtonTitleConfiguration {
+    /// 메인 컬러 텍스트 설정
     static let textPrimary = ButtonTitleConfiguration(
-        font: .systemFont(ofSize: 28, weight: .bold),
+        font: .systemFont(ofSize: 14, weight: .bold),
         normalColor: .accent,
         highlightedColor: .white,
         disabledColor: .gray500,
@@ -58,10 +75,10 @@ extension ButtonTitleConfiguration {
 
 //MARK: - Setting ButtonAppearanceConfiguration
 extension ButtonAppearanceConfiguration {
-    /// 카테고리
-    static let textPrimary = ButtonAppearanceConfiguration(
+    /// 타이틀만 표출되는 디자인
+    static let textOnly = ButtonAppearanceConfiguration(
         backgroundColor: UIColor.clear,
-        highlightBackgroundColor: UIColor.clear,
+        highlightedBackgroundColor: UIColor.clear,
         disabledBackgroundColor: UIColor.clear,
         cornerRadius: 0,
         borderWidth: 0,
