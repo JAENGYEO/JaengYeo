6490241a9160184a799590844ebd015a7fb0eccf
Bin <ybin4548@gmail.com>
2026-04-08 17:56:54 +0900

    ✨ feat: RegisterFormData 구조체 추가

---
+++ JaengYeo/JaengYeo/Model/Domain/Register/RegisterFormData.swift
@@ -0,0 +1,26 @@
+//
+//  RegisterFormData.swift
+//  JaengYeo
+//
+//  Created by 손영빈 on 4/8/26.
+//
+
+import Foundation
+
+//TODO: 추후 Entity 저장 로직 구현 시 수정 필요
+struct RegisterFormData {
+    var name: String?
+    var mainCategory: String?
+    var midCategory: UUID?
+    var subCategory: UUID?
+    var quantity: Int?
+    var quantityUnit: String?
+    var price: Int?
+    var purchaseDate: Date?
+    var expiryDate: Date?
+    var locationMemo: String?
+    var memo: String?
+    var imageBase64: String?
+    var isLowStockNotificationEnabled: Bool?
+    var lowStockThreshold: Int?
+}
