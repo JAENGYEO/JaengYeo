ecb0eca192f2a6f70e06a4483590f0e29e6d6ed2
hanjuheon <godzx3@gmail.com>
2026-04-09 21:56:19 +0900

    🌟feat: StockViewController 구현 및 재고 화면 UI 구성
    - StockViewModel을 주입받는 StockViewController 추가
    - 메인 카테고리 세그먼트 컨트롤 UI 구성
    - CategoryFilterView, ProductCollectionView를 재고 화면에 배치
    - RxSwift 바인딩으로 메인 카테고리 세그먼트 데이터 연결
    - 재고현황 네비게이션 타이틀 및 검색 버튼 설정
    
    ref #17

---
+++ JaengYeo/JaengYeo/View/Stock/StockViewController.swift
@@ -0,0 +1,138 @@
+//
+//  StockViewController.swift
+//  JaengYeo
+//
+//  Created by 손영빈 on 4/7/26.
+//
+
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+import UIKit
+
+final class StockViewController: UIViewController {
+
+    //MARK: - ViewModel
+    var viewModel: StockViewModel
+
+    //MARK: - Properties
+    var disposeBag = DisposeBag()
+    
+
+    //MARK: - Components
+    let mainCategorySegment = UISegmentedControl().then {
+        let attributes: [NSAttributedString.Key: Any] = [
+            .font: LabelConfiguration.bodyMedium14.font,
+            .foregroundColor: UIColor.black
+        ]
+        let selectAttributes:
+        [NSAttributedString.Key: Any] = [
+           .font: LabelConfiguration.bodyMedium14.font,
+           .foregroundColor: UIColor.white
+       ]
+        $0.setTitleTextAttributes(attributes, for: .normal)
+        $0.setTitleTextAttributes(selectAttributes, for: .selected)
+
+        $0.selectedSegmentTintColor = .accent
+    }
+
+    let categotyFilterView = CategoryFilterView()
+    
+    let productCollectionView = ProductCollectionView()
+    
+
+    init(viewModel: StockViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationBarConfigure()
+        Bind()
+        configureUI()
+    }
+}
+
+//MARK: - Binding
+extension StockViewController {
+    func Bind() {
+        viewModel.mainCategory
+            .subscribe(onNext: {
+                [weak self] items in
+                guard let self else { return }
+                self.mainCategorySegment.removeAllSegments()
+                items.enumerated().forEach { index, title in
+                    self.mainCategorySegment.insertSegment(
+                        withTitle: title,
+                        at: index,
+                        animated: false
+                    )
+                }
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+extension StockViewController {
+    @objc
+    private func didTapSearchButton() {
+    }
+}
+
+//MARK: - Configure UI
+extension StockViewController {
+    private func navigationBarConfigure() {
+        title = "재고현황"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "magnifyingglass"),
+            style: .plain,
+            target: self,
+            action: #selector(didTapSearchButton)
+        )
+    }
+    
+    private func configureUI() {
+        view.addSubview(mainCategorySegment)
+        view.addSubview(categotyFilterView)
+        view.addSubview(productCollectionView)
+        mainCategorySegment.selectedSegmentIndex = 0
+
+        mainCategorySegment.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(8)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(40)
+        }
+        categotyFilterView.snp.makeConstraints {
+            $0.top.equalTo(mainCategorySegment.snp.bottom)
+            $0.leading.equalToSuperview().offset(16)
+            $0.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(46)
+        }
+        productCollectionView.snp.makeConstraints {
+            $0.top.equalTo(categotyFilterView.snp.bottom)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.bottom.equalToSuperview()
+        }
+    }
+}
+
+#Preview {
+    let nav = UINavigationController(rootViewController: StockViewController(viewModel: StockViewModel()))
+    
+    let selectAttributes:
+    [NSAttributedString.Key: Any] = [
+       .font: LabelConfiguration.titleSemi18.font,
+       .foregroundColor: LabelConfiguration.titleSemi18.color
+   ]
+    
+    nav.navigationBar.titleTextAttributes = selectAttributes
+    return nav
+}
