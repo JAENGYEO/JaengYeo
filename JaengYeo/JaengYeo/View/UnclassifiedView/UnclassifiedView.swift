a839aa4b2f0813a8fe03c1f261d7ab34f7db5b68
Bin <ybin4548@gmail.com>
2026-04-16 10:22:58 +0900

    ✨ feat: ProductCell unclassifiedType 추가 #75

---
+++ JaengYeo/JaengYeo/View/UnclassifiedView/UnclassifiedView.swift
@@ -0,0 +1,76 @@
+//
+//  UnclassifiedView.swift
+//  JaengYeo
+//
+//  Created by 손영빈 on 4/15/26.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+final class UnclassifiedView: UIView {
+    
+    let infoView = UIView().then {
+        $0.backgroundColor = .gray50
+    }
+    
+    let infoLabel = UILabel().then {
+        $0.text = "분류가 덜 된 상품들의 세부내용을 입력해주세요"
+        $0.font = LabelConfiguration.body12.font
+        $0.textColor = .gray300
+        $0.textAlignment = .center
+    }
+    
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout()).then {
+        $0.backgroundColor = .gray50
+        $0.showsVerticalScrollIndicator = false
+        $0.register(ProductCell.self, forCellWithReuseIdentifier: ProductCell.id)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configCollectionViewLayout()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension UnclassifiedView {
+    private func setLayout() {
+        backgroundColor = .white
+        infoView.addSubview(infoLabel)
+        [infoView, collectionView].forEach { addSubview($0) }
+        
+        infoView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+        }
+        infoLabel.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(12)
+            $0.leading.trailing.equalToSuperview()
+        }
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(infoView.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
+
+extension UnclassifiedView {
+    private func configCollectionViewLayout() {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, environment in
+            var config = UICollectionLayoutListConfiguration(appearance: .plain)
+            config.showsSeparators = false
+            config.backgroundColor = .clear
+            let section = NSCollectionLayoutSection.list(using: config, layoutEnvironment: environment )
+            section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 16, bottom: 8, trailing: 16)
+            section.interGroupSpacing = 8
+            return section
+        }
+        collectionView.setCollectionViewLayout(layout, animated: false)
+    }
+}
