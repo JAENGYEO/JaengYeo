9fdac1b444cbab4a1b096048caecffd7ba0c914d
Bin <ybin4548@gmail.com>
2026-04-20 20:49:16 +0900

    o################ ♻️ refactor: gpt일 경우 디코딩 로직 강화

--- JaengYeo/JaengYeo/Model/DTO/Supabase/AIResponse.swift
+++ JaengYeo/JaengYeo/Model/DTO/Supabase/AIResponse.swift
@@ -15,4 +15,22 @@ struct AIResponseItem: Decodable {
     let name: String
     let estimatedCategory: String?
     let estimatedQuantity: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case name, estimatedCategory, estimatedQuantity
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        estimatedCategory = try container.decodeIfPresent(String.self, forKey: .estimatedCategory)
+
+        if let intValue = try? container.decodeIfPresent(Int.self, forKey: .estimatedQuantity) {
+            estimatedQuantity = intValue
+        } else if let strValue = try? container.decodeIfPresent(String.self, forKey: .estimatedQuantity) {
+            estimatedQuantity = Int(strValue.filter { $0.isNumber })
+        } else {
+            estimatedQuantity = nil
+        }
+    }
 }
