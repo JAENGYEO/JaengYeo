1038ccca5f52ef18d3d6a7f8aa5bc44170c64b3c
Bin <ybin4548@gmail.com>
2026-04-20 16:15:49 +0900

    ♻️ refactor: Constants.Dev 삭제

--- JaengYeo/JaengYeo/Utils/Constants.swift
+++ JaengYeo/JaengYeo/Utils/Constants.swift
@@ -8,9 +8,6 @@
 import Foundation
 
 enum Constants {
-    enum Dev {
-        static let userId = UUID(uuidString: "9b6fb054-73e5-478a-9b84-0ad49ff984bb")! // 로그인 기능 구현 시 수정 필요
-    }
     enum Supabase {
         static let url = (Bundle.main.infoDictionary?["SUPABASE_URL"] as? String ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
         static let anonKey = (Bundle.main.infoDictionary?["SUPABASE_ANON_KEY"] as? String ?? "").trimmingCharacters(in: .whitespacesAndNewlines)
