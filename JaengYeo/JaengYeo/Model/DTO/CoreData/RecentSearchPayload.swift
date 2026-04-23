584351cff3ff546a1ccf7e28edc43cbc65663fb4
Bin <ybin4548@gmail.com>
2026-04-23 12:52:43 +0900

    ✨ feat: RecentSearchEntity CoreData Entity 추가 #132

---
+++ JaengYeo/JaengYeo/Model/DTO/CoreData/RecentSearchPayload.swift
@@ -0,0 +1,8 @@
+//
+//  RecentSearchPayload.swift
+//  JaengYeo
+//
+//  Created by 손영빈 on 4/23/26.
+//
+
+import Foundation
---
+++ JaengYeo/RecentSearchEntity+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  RecentSearchEntity+CoreDataClass.swift
+//  JaengYeo
+//
+//  Created by 손영빈 on 4/23/26.
+//
+//
+
+public import Foundation
+public import CoreData
+
+public typealias RecentSearchEntityCoreDataClassSet = NSSet
+
+@objc(RecentSearchEntity)
+public class RecentSearchEntity: NSManagedObject {
+
+}
