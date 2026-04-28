64041cfcd386f419eaff9703a9a21291513687f2
hanjuheon <godzx3@gmail.com>
2026-04-28 19:30:05 +0900

    🌟feat: 장바구니 CoreData 엔티티 추가
    - CartEntity CoreData 모델 추가
    - 장바구니 상품 식별자, 참조 상품 ID, 상품명, 대분류, 생성일 필드 구성
    - CartEntity 수동 CoreData 클래스 및 properties 파일 추가
    - CoreData 자동 생성과 수동 생성 파일이 중복되지 않도록 엔티티 설정 정리
    
    ref #148

---
+++ JaengYeo/JaengYeo/Data/CoreData/CartEntity+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  CartEntity+CoreDataClass.swift
+//  JaengYeo
+//
+//  Created by Hanjuheon on 4/28/26.
+//
+//
+
+import Foundation
+import CoreData
+
+public typealias CartEntityCoreDataClassSet = NSSet
+
+@objc(CartEntity)
+public class CartEntity: NSManagedObject {
+
+}
---
+++ JaengYeo/JaengYeo/Data/CoreData/CartEntity+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  CartEntity+CoreDataProperties.swift
+//  JaengYeo
+//
+//  Created by Hanjuheon on 4/28/26.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public typealias CartEntityCoreDataPropertiesSet = NSSet
+
+extension CartEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CartEntity> {
+        return NSFetchRequest<CartEntity>(entityName: "CartEntity")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var referenceId: UUID?
+    @NSManaged public var name: String?
+    @NSManaged public var mainCategory: String?
+    @NSManaged public var createDate: Date?
+
+}
+
+extension CartEntity : Identifiable {
+
+}
