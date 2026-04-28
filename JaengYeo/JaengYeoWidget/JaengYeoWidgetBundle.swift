13b7cd610450f6925927372b439b1d1ac8117da3
Bin <ybin4548@gmail.com>
2026-04-28 17:26:45 +0900

    ✨ feat: Widget Extension 타깃 추가 #147
    - JaengYeoWidget 타깃 신규 생성 (SwiftUI + WidgetKit)
    - Bundle ID: com.jaengyoeo.JaengYeo.JaengYeoWidget
    - iOS 17.0 이상, iPhone 전용
    - 본 앱과 동일한 수동 서명 (Provisioning Profile 별도 발급)
    - Configuration App Intent 포함, Live Activity / Control 제외

---
+++ JaengYeo/JaengYeoWidget/JaengYeoWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  JaengYeoWidgetBundle.swift
+//  JaengYeoWidget
+//
+//  Created by 손영빈 on 4/28/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct JaengYeoWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        JaengYeoWidget()
+    }
+}
