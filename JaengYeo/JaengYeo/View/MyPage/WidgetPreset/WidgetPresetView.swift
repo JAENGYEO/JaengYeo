0caf92bfaf0011fb633d75c88a65401c362011a4
Bin <ybin4548@gmail.com>
2026-04-30 12:21:58 +0900

    ✨ feat: WidgetPresetView 추가 #147

---
+++ JaengYeo/JaengYeo/View/MyPage/WidgetPreset/WidgetPresetView.swift
@@ -0,0 +1,60 @@
+//
+//  WidgetPresetView.swift
+//  JaengYeo
+//
+//  Created by 손영빈 on 4/30/26.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+enum WidgetPresetSection: Int, CaseIterable {
+    case main
+}
+
+final class WidgetPresetView: UIView {
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout()).then {
+        $0.backgroundColor = .white
+        $0.showsVerticalScrollIndicator = false
+        $0.register(WidgetPresetCell.self, forCellWithReuseIdentifier: WidgetPresetCell.id)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension WidgetPresetView {
+    private func setLayout() {
+        addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
+
+extension WidgetPresetView {
+    private func createLayout() -> UICollectionViewLayout {
+        UICollectionViewCompositionalLayout { _, _ in
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(56))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(56))
+            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 12, leading: 16, bottom: 16, trailing: 16)
+            section.interGroupSpacing = 8
+            return section
+        }
+    }
+}
