2ba8db774836981294b9c0669af7cc7317f4a46c
Bin <ybin4548@gmail.com>
2026-05-01 15:27:55 +0900

    ✨ feat: QuantityWidgetIntent 추가 #147
    WidgetConfigurationIntent로 프리셋 선택 파라미터 정의.

--- JaengYeo/JaengYeoWidget/QuantityWidgetIntent.swift
+++ JaengYeo/JaengYeoWidget/QuantityWidgetIntent.swift
@@ -5,4 +5,13 @@
 //  Created by 손영빈 on 5/1/26.
 //
 
-import Foundation
+import AppIntents
+import WidgetKit
+
+struct QuantityWidgetIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "수량 위젯"
+    static var description: IntentDescription = "프리셋을 선택하여 상품 수량을 확인합니다"
+    
+    @Parameter(title: "프리셋")
+    var preset: WidgetPresetAppEntity?
+}
