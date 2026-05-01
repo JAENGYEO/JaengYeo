c392c33742475d5724bfb2a6996db8dbb1961a1e
Bin <ybin4548@gmail.com>
2026-05-01 16:03:57 +0900

    ✨ feat: DecreaseQuantityIntent 수량 감소 AppIntent 추가 #147
    위젯 - 버튼 탭 시 CoreData 수량 -1 업데이트 + 타임라인 갱신.
    수량 1일 때는 View에서 Link(confirmDelete)로 대체하여 Intent 미호출.

--- JaengYeo/JaengYeoWidget/DecreaseQuantityIntent.swift
+++ JaengYeo/JaengYeoWidget/DecreaseQuantityIntent.swift
@@ -5,4 +5,27 @@
 //  Created by 손영빈 on 5/1/26.
 //
 
-import Foundation
+import WidgetKit
+import AppIntents
+
+struct DecreaseQuantityIntent: AppIntent {
+    static var title: LocalizedStringResource = "수량 감소"
+    
+    @Parameter(title: "상품 ID")
+    var productID: String
+    
+    init() {}
+    
+    init(productID: UUID) {
+        self.productID = productID.uuidString
+    }
+    
+    func perform() async throws -> some IntentResult {
+        guard let id = UUID(uuidString: productID) else { return .result() }
+        await MainActor.run {
+            _ = WidgetDataStore().updateQuantity(productID: id, delta: -1)
+        }
+        WidgetCenter.shared.reloadAllTimelines()
+        return .result()
+    }
+}
